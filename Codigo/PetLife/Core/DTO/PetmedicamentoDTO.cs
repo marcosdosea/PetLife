38b0c08f9bfe8581fa85c973b4ff808ca4a45519
paulovsnts <paulovitormengao2015@gmail.com>
2023-09-04 08:52:18 -0300

    Implementação feita. Resolved #207

--- Codigo/PetLife/Core/DTO/PetmedicamentoDTO.cs
+++ Codigo/PetLife/Core/DTO/PetmedicamentoDTO.cs
@@ -8,5 +8,12 @@ namespace Core.DTO
 {
     public class PetmedicamentoDTO
     {
+        public uint IdPet { get; set; }
+
+        public uint IdMedicamento { get; set; }
+
+        public uint Dosagem { get; set; }
+
+        public string Medida { get; set; } = null!;
     }
 }
---
+++ Codigo/PetLife/Core/Service/IMedicamentoService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IMedicamentoService
+    {
+        public int Create(Medicamento medicamento);
+        public void Edit(Medicamento medicamento);
+        public void Delete(int id);
+        public Medicamento Get(int id);
+        public IEnumerable<MedicamentoDTO> GetAll();
+        public IEnumerable<MedicamentoDTO> GetByNome(string nome);
+    }
+}
