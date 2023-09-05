fc758c3fb86d1de56e2189f4ee4f5299831e339f
Natan <natanbola17@gmail.com>
2023-09-04 22:14:35 -0300

    DTO e Service de Manter Pet implementados. Rsolved #206

---
+++ Codigo/PetLife/Core/Service/IPetService.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPetService
+    {
+        public int Create(Pet pet);
+        public void Edit(Pet pet);
+        public void Delete(int id);
+        public Pet Get(int id);
+        public IEnumerable<PetDTO> GetAll();
+        public IEnumerable<PetDTO> GetAll(string nome);
+    }
+}
