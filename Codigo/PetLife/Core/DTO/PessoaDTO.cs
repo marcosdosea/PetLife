80071dc7e06767284788b1d0d041782c120688ad
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-09-04 21:35:09 -0300

    Add files via upload

--- Codigo/PetLife/Core/DTO/PessoaDTO.cs
+++ Codigo/PetLife/Core/DTO/PessoaDTO.cs
@@ -8,5 +8,22 @@ namespace Core.DTO
 {
     public class PessoaDTO
     {
+        public string Nome { get; set; } = null!;
+
+        public DateTime DataNascimento { get; set; }
+
+        public string Telefone { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string Estado { get; set; } = null!;
+
+        public string Cidade { get; set; } = null!;
+
+        public string? Rua { get; set; }
+
+        public string? Numero { get; set; }
+
+        public int? Cep { get; set; }
     }
 }
