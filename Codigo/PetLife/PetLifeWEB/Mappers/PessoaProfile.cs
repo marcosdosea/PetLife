d57b136a87505b57b4bbe1b3455a988590ebf189
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-09-08 18:13:26 -0300

    Pessoa Mappers - Resolved e Pasta Mappers - Criada

---
+++ Codigo/PetLife/PetLifeWEB/Mappers/PessoaProfile.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using AutoMapper;
+using PetLifeWEB.Models;
+
+namespace PetLifeWEB.Mappers
+{
+    public class PessoaProfile : Profile
+    {
+        public PessoaProfile()
+        {
+            CreateMap<PessoaModel, Pessoa>().ReverseMap();
+        }
+    }
+}
