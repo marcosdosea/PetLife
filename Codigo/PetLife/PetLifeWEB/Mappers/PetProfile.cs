022ca90aec114b075b549e4ab42077540b2aa241
Natan <natanbola17@gmail.com>
2023-09-09 14:32:58 -0300

    Implementação das ViewsModel e Mappers de Manter Pet. Resolved #210

---
+++ Codigo/PetLife/PetLifeWEB/Mappers/PetProfile.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using AutoMapper;
+using PetLifeWEB.Models;
+
+namespace PetLifeWEB.Mappers
+{
+    public class PetProfile : Profile
+    {
+        public PetProfile()
+        { 
+            CreateMap<PetModel, Pet>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/PetLife/PetLifeWEB/Models/PetModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PetLifeWEB.Models
+{
+    public class PetModel
+    {
+        [Display(Name = "Nome do pet")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(30, MinimumLength = 1, ErrorMessage = "Nome do pet deve conter entre 1 e 30 caracteres")]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Espécie")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "Espécie deve conter entre 1 e 15 caracteres")]
+        public string Especie { get; set; } = null!;
+
+        [Display(Name = "Raça")]
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "Raça deve conter entre 1 e 15 caracteres")]
+        public string Raca { get; set; } = null!;
+
+        [Display(Name = "Data de Nascimento")]
+        [DataType(DataType.Date, ErrorMessage = "Campo obrigatório")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Sexo")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(1, ErrorMessage = "Sexo deve conter apenas 1 caractere")]
+        public string Sexo { get; set; } = null!;
+
+        [Display(Name = "Peso")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public double Peso { get; set; }
+    }
+}
