ddd6ac43cb21fe29960be05efca8b149402c87ea
EDMARQUES ANDRADE COSTA <edandrade4040@gmail.com>
2023-09-11 09:53:11 -0300

    ViewModels e Mappers de Gerir Vendas implementados. #227Resolved

---
+++ Codigo/PetLife/PetLifeWEB/Mappers/VedumProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core;
+using PetLifeWEB.Models;
+
+namespace PetLifeWEB.Mappers
+{
+    public class VendumProfile : Profile
+    {
+        public VendumProfile()
+        {
+            CreateMap<VendumModel, Pet>().ReverseMap();
+        }
+    }
+
+}
---
+++ Codigo/PetLife/PetLifeWEB/Models/VendumModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetLifeWEB.Models
+{
+    public class VendumModel
+    {
+        [Display(Name = "Codigo")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Id { get; set; } = null!;
+
+        [Display(Name = "Data de Venda")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public DateTime DataVenda { get; set; }
+
+        [Display(Name = "Forma de Pagamento")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string FormaPagamento { get; set; } = null!;
+        
+        [Display(Name = "Parcelas")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public uint? Parcelas { get; set; }
+
+        [Display(Name = "Pago")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public sbyte Pago { get; set; }
+    }
+}
