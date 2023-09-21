20eae8143470d6191c264500db4cff41cc897755
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-09-21 20:44:01 -0300

    Created in PetLife - PetLifeWEBTest & ServiceTest

---
+++ Codigo/PetLife/PetLifeWEBTests/Controllers/PetControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PetLifeWEB.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetLifeWEB.Controllers.Tests
+{
+    [TestClass()]
+    public class PetControllerTests
+    {
+        [TestMethod()]
+        public void PetControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
