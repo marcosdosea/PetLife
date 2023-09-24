28970f0c9be36004b75e5a59f71a458b968a65b1
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-09-24 19:35:16 -0300

    IPetshopService - Criado

---
+++ Codigo/PetLife/Core/Service/IPetshopService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IPetshopService
+    {
+        public uint Create(Petshop petshop);
+        public void Edit(Petshop petshop);
+        public void Delete(uint id);
+        public Petshop Get(uint id);
+        public IEnumerable<Petshop> GetAll();
+        public IEnumerable<Petshop> GetbyNome(string nome);
+    }
+}
