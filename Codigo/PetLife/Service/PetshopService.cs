4b5f42602bfbacb8cd484c8d255645ef331ebda7
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-09-24 19:36:20 -0300

    PetshopService Criado

---
+++ Codigo/PetLife/Service/PetshopService.cs
@@ -0,0 +1,60 @@
+﻿using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Core;
+
+namespace Service
+{
+    public class PetshopService :IPetshopService
+    {
+        private readonly PetLifeContext context;
+
+        public PetshopService(PetLifeContext context)
+        {
+            this.context = context;
+        }
+
+        public uint Create(Petshop petshop)
+        {
+            context.Add(petshop);
+            context.SaveChanges();
+            return (uint)petshop.Id;
+        }
+
+        public void Edit(Petshop petshop) 
+        { 
+            context.Update(petshop);
+            context.SaveChanges();
+        }
+
+        public void Delete(uint id) 
+        {
+            var petshop = context.Petshops.Find(id);
+            if(petshop != null ) 
+            {
+                context.Petshops.Remove(petshop);
+                context.SaveChanges();
+            }
+        }
+
+        public Petshop Get(uint id)
+        {
+            return context.Petshops.Find(id);
+        }
+
+        public IEnumerable<Petshop> GetAll()
+        {
+            return context.Petshops.AsNoTracking();
+        }
+
+        public IEnumerable<Petshop> GetbyNome(string nome)
+        {
+            return context.Petshops.Where(
+                petshop => petshop.Nome.StartsWith(nome)).AsNoTracking();
+        }
+    }
+}
