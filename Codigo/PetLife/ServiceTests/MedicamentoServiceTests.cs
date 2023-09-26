2f94726adc56e7fa547c11317e6b7fa9579513a5
paulovsnts <paulovitormengao2015@gmail.com>
2023-09-25 21:36:14 -0300

    Testes criados para MedicamentoService. Resolved #278

--- Codigo/PetLife/ServiceTests/MedicamentoServiceTests.cs
+++ Codigo/PetLife/ServiceTests/MedicamentoServiceTests.cs
@@ -1,56 +1,108 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core;
+using Core.Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Service.Tests
 {
     [TestClass()]
     public class MedicamentoServiceTests
     {
-        [TestMethod()]
-        public void MedicamentoServiceTest()
+        private PetLifeContext _context;
+        private IMedicamentoService _medicamentoService;
+
+        [TestInitialize]
+        public void Initialize()
         {
-            Assert.Fail();
+            //Arrange
+            var builder = new DbContextOptionsBuilder<PetLifeContext>();
+            builder.UseInMemoryDatabase("PetLife");
+            var options = builder.Options;
+
+            _context = new PetLifeContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Database.EnsureCreated();
+            var medicamentos = new List<Medicamento>
+                {
+                    new Medicamento { Id = 1, Nome = "Dipirona", Importado = "Sim"},
+                    new Medicamento { Id = 2, Nome = "Escabin", Importado = "Não"},
+                    new Medicamento { Id = 3, Nome = "Ibuprofeno", Importado = "Sim"},
+                };
+
+            _context.AddRange(medicamentos);
+            _context.SaveChanges();
+
+            _medicamentoService = new MedicamentoService(_context);
         }
 
         [TestMethod()]
         public void CreateTest()
         {
-            Assert.Fail();
+            // Act
+            _medicamentoService.Create(new Medicamento() { Id = 4, Nome = "Tylenol", Importado = "Sim"});
+            // Assert
+            Assert.AreEqual(4, _medicamentoService.GetAll().Count());
+            var medicamento = _medicamentoService.Get(4);
+            Assert.AreEqual("Tylenol", medicamento.Nome);
         }
 
         [TestMethod()]
         public void DeleteTest()
         {
-            Assert.Fail();
+            // Act
+            _medicamentoService.Delete(2);
+            // Assert
+            Assert.AreEqual(2, _medicamentoService.GetAll().Count());
+            var medicamento = _medicamentoService.Get(2);
+            Assert.AreEqual(null, medicamento);
         }
 
         [TestMethod()]
         public void EditTest()
         {
-            Assert.Fail();
+            //Act 
+            var medicamento = _medicamentoService.Get(3);
+            medicamento.Nome = "Paracetamol";
+            medicamento.Importado = "Não";
+            _medicamentoService.Edit(medicamento);
+            //Assert
+            medicamento = _medicamentoService.Get(3);
+            Assert.IsNotNull(medicamento);
+            Assert.AreEqual("Paracetamol", medicamento.Nome);
+            Assert.AreEqual("Não", medicamento.Importado);
         }
 
         [TestMethod()]
         public void GetTest()
         {
-            Assert.Fail();
+            var medicamento = _medicamentoService.Get(1);
+            Assert.IsNotNull(medicamento);
+            Assert.AreEqual("Dipirona", medicamento.Nome);
+            Assert.AreEqual("Sim", medicamento.Importado);
         }
 
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            // Act
+            var listaMedicamento = _medicamentoService.GetAll();
+            // Assert
+            Assert.IsInstanceOfType(listaMedicamento, typeof(IEnumerable<Medicamento>));
+            Assert.IsNotNull(listaMedicamento);
+            Assert.AreEqual(3, listaMedicamento.Count());
+            Assert.AreEqual((uint)1, listaMedicamento.First().Id);
+            Assert.AreEqual("Dipirona", listaMedicamento.First().Nome);
         }
 
         [TestMethod()]
         public void GetByNomeTest()
         {
-            Assert.Fail();
+            //Act
+            var medicamentos = _medicamentoService.GetByNome("Dipirona");
+            //Assert
+            Assert.IsNotNull(medicamentos);
+            Assert.AreEqual(1, medicamentos.Count());
+            Assert.AreEqual("Dipirona", medicamentos.First().Nome);
         }
     }
 }
