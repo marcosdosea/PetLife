e81cbc2840ac7a2884e1f45f481f16f22248a134
paulovsnts <paulovitormengao2015@gmail.com>
2023-09-26 00:03:54 -0300

    Model, mapper, controller e views de produto implementadas. Resolved #282

---
+++ Codigo/PetLife/PetLifeWEB/Models/ProdutoModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace PetLifeWEB.Models
+{
+    public class ProdutoModel
+    {
+        [Display(Name = "Id")]
+        [Required(ErrorMessage = "Id do produto é obrigatório")]
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(40, MinimumLength = 5, ErrorMessage = "Nome do Produto deve ter entre 5 e 40 caracteres")]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "Código do produto é obrigatório")]
+        public uint Codigo { get; set; }
+
+        [Display(Name = "Ativo")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public sbyte Ativo { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public uint Quantidade { get; set; }
+
+        [Display(Name = "Descrição")]
+        [StringLength(400, MinimumLength = 5, ErrorMessage = "A descrição do Produto deve ter no mínimo 5 e no máximo 400 caracteres")]
+        public string Descricao { get; set; } = null!;
+
+        [Display(Name = "IdPetshop")]
+        [Required(ErrorMessage = "Id do petshop é obrigatório")]
+        [Key]
+        public uint IdPetshop { get; set; }
+    }
+}
