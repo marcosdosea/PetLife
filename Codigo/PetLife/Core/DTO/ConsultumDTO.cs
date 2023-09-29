9ac7678285cb4c933ed079bb354beb55f376b49b
Natan <natanbola17@gmail.com>
2023-09-29 16:25:40 -0300

    Implementação do caso de uso Manter Consulta. Resolved #290

--- Codigo/PetLife/Core/DTO/ConsultumDTO.cs
+++ Codigo/PetLife/Core/DTO/ConsultumDTO.cs
@@ -9,14 +9,10 @@ namespace Core.DTO
     public class ConsultumDTO
     {
         public DateTime DataAgendamento { get; set; }
-
         public DateTime DataConsulta { get; set; }
-
-        public string Status { get; set; }
-        public string NomeCliente { get; set; }
-
-        public string NomePet { get; set; }
-
-        public string Descricao { get; set; } = null!;
+        public string? Status { get; set; }
+        public string? NomeCliente { get; set; }
+        public string? NomePet { get; set; }
+        public string? Descricao { get; set; } = null!;
     }
 }
