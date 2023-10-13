023bde79cc295a8f46ef60664d4eb37bb1290f0f
Natan <natanbola17@gmail.com>
2023-10-13 20:46:42 -0300

    Implementação do caso de uso Consulta. Resolved #296

---
+++ Codigo/PetLife/PetLifeWEB/Mappers/ConsultumProfile.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using AutoMapper;
+using PetLifeWEB.Models;
+
+namespace PetLifeWEB.Mappers
+{
+    public class ConsultumProfile : Profile
+    {
+        public ConsultumProfile()
+        {
+            CreateMap<ConsultumModel, Consultum>().ReverseMap();
+        }
+    }
+}
