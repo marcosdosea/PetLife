5e406918585b3fd0b0c8afb521c9b0d3aecbde4c
paulovsnts <paulovitormengao2015@gmail.com>
2023-10-16 00:53:02 -0300

    Implementada API para medicamento. Resolved #313

---
+++ Codigo/PetLife/PetLifeAPI/Mappers/MedicamentoProfile.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+using AutoMapper;
+using PetLifeWEB.Models;
+
+namespace PetLifeAPI.Mappers
+{
+    public class MedicamentoProfile : Profile
+    {
+        public MedicamentoProfile()
+        {
+            CreateMap<MedicamentoModel, Medicamento>().ReverseMap();
+        }
+    }
+}
