bc9772f7fec53fbe5f57ceeca677ccf108479a26
ValdirMS <59018974+ValdirMS@users.noreply.github.com>
2023-10-16 19:24:21 -0300

    Criação para Geração de Autenticação e Autorização

--- Codigo/PetLife/PetLifeWEB/Program.cs
+++ Codigo/PetLife/PetLifeWEB/Program.cs
@@ -2,6 +2,9 @@ using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
 using Service;
+using Microsoft.AspNetCore.Identity;
+using PetLifeWEB.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace PetLifeWeb
 {
@@ -27,6 +30,44 @@ namespace PetLifeWeb
 
             builder.Services.AddDbContext<PetLifeContext>(
                 options => options.UseMySQL(builder.Configuration.GetConnectionString("PetLifeDatabase")));
+            builder.Services.AddDbContext<IdentityContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("PetLifeDatabase")));
+
+            builder.Services.AddDefaultIdentity<UsuarioIdentity>(options => {
+                //SignIn settings
+                options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                //Password settings
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequiredLength = 6;
+
+                //Default User settings
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+
+                //Default Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+            }).AddRoles<IdentityRole>().AddEntityFrameworkStores<IdentityContext>();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.AccessDeniedPath = "/Identity/Account/AcessDenied";
+                options.Cookie.Name = "YourAppCookieName";
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                options.LoginPath = "/Identity/Account/Login";
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.SlidingExpiration = true;
+
+            });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -41,8 +82,11 @@ namespace PetLifeWeb
             app.UseStaticFiles();
 
             app.UseRouting();
-
+            
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",
